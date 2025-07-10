3cfcaac87447de8890977dbb75841837ab4b7854
Subomi Obans <subomi.obans@gmail.com>
2025-07-10 08:39:28 +0100

    update

--- pra.py
+++
@@ -1,9 +0,0 @@
-temperature = int(input("What is the temperature over there? "))
-
-if temperature > 30:
-    print("its a hot day")
-elif temperature < 10:
-    print("its a bit cold")
-elif temperature > 20:
-    print("its a nice day")
-    print("care to take a walk")
